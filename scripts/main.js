8a7b6b194fa2d353b29b63652f4f7d85285b12ef
anmenmor <menendezandrea86@gmail.com>
2019-11-17 23:50:50 +0100

    modify and add new files

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+function foo () {
+    var foo = document.getElementsByName("fechas[]")
+   // var asdf = foo.filter(input => input.name = "fechas[]")
+    console.dir(foo)
+}
+
+function suma (a, b) {
+     a + b
+}
