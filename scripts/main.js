ffeea81714633eb0b6faaebc425e286dee353d88
anmenmor <menendezandrea86@gmail.com>
2019-11-20 23:24:58 +0100

    modify files

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,25 @@
-function foo () {
-    var foo = document.getElementsByName("fechas[]")
-   // var asdf = foo.filter(input => input.name = "fechas[]")
-    console.dir(foo)
-}
+function checkboxValidation(language) {
+  let checkboxes = Array.from(document.getElementsByName("dates[]"));
+  checkboxes.forEach(
+    checkbox =>
+      (checkbox.onchange = () => {
+        switch (checkboxes.filter(v => v.checked == true).length) {
+          case 0:
+            checkboxes.forEach(x => (x.required = true));
+            break;
+          case 1:
+            checkboxes.forEach(x => (x.required = false));
+            break;
+          case 3:
+            if (language == "es") {
+              alert("Solo se pueden seleccionar dos fechas como máximo");
+            } else {
+              alert("Only two dates can be selected at most");
+            }
 
-function suma (a, b) {
-     a + b
+            checkbox.checked = false;
+            break;
+        }
+      })
+  );
 }
